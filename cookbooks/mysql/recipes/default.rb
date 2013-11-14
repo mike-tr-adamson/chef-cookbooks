79fd6e3f69f0af3b426d22dce5b47fe416bbf8ba
Mike Adamson <mikeatdot@gmail.com>
2013-11-14 16:46:27 +0000

    Changes to databags

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -7,14 +7,14 @@
 # All rights reserved - Do Not Redistribute
 #
 
-mysql_password = Chef::EncryptedDataBagItem.load("passwords", "mysql")
+mysql = Chef::EncryptedDataBagItem.load("passwords", "mysql")
 
 bash 'Install mysql server' do
   user 'root'
   group 'root'
   code <<-EOH
-  debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password #{mysql_password}"
-  debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password #{mysql_password}"
+  debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password #{mysql.password}"
+  debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password #{mysql.password}"
   apt-get -y install mysql-server	
   EOH
   not_if { ::File.exists?('/etc/init.d/mysql') }
--- cookbooks/openswan/recipes/default.rb
+++ cookbooks/openswan/recipes/default.rb
@@ -6,6 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+openswan = Chef::EncryptedDataBagItem.load("passwords", "openswan")
+
 bash 'Install openswan' do
   user 'root'
   group 'root'
